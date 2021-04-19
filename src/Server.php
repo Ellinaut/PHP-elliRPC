1fd7ff102a7802b6eb3eab1fb3bdd6ca8321e620
pmarien <philipp.marien@gmail.com>
2021-04-19 23:49:02 +0200

    fixed error responses

--- src/Server.php
+++ src/Server.php
@@ -7,13 +7,17 @@ use Ellinaut\ElliRPC\DataTransfer\Response\ExceptionResponse;
 use Ellinaut\ElliRPC\Event\RequestParsed;
 use Ellinaut\ElliRPC\Exception\MissingRequestException;
 use Ellinaut\ElliRPC\Exception\MissingResponseException;
+use Ellinaut\ElliRPC\Exception\StatusProvidingExceptionInterface;
 use Ellinaut\ElliRPC\Exception\UnsupportedResponseException;
 use Ellinaut\ElliRPC\RequestParser\RequestParserInterface;
 use Ellinaut\ElliRPC\RequestProcessor\RequestProcessorInterface;
+use Ellinaut\ElliRPC\ResponseFactory\ExceptionMapperTrait;
 use Ellinaut\ElliRPC\ResponseFactory\ResponseFactoryInterface;
 use Psr\EventDispatcher\EventDispatcherInterface;
 use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseFactoryInterface as PsrResponseFactory;
 use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamFactoryInterface;
 use Throwable;
 
 /**
@@ -21,6 +25,8 @@ use Throwable;
  */
 class Server
 {
+    use ExceptionMapperTrait;
+
     /**
      * @var EventDispatcherInterface
      */
@@ -42,26 +48,43 @@ class Server
     private ResponseFactoryInterface $responseFactory;
 
     /**
+     * @var PsrResponseFactory
+     */
+    private PsrResponseFactory $psrResponseFactory;
+
+    /**
+     * @var StreamFactoryInterface
+     */
+    private StreamFactoryInterface $streamFactory;
+
+    /**
      * @param EventDispatcherInterface $eventDispatcher
      * @param RequestParserInterface $requestParser
      * @param RequestProcessorInterface $requestProcessor
      * @param ResponseFactoryInterface $responseFactory
+     * @param PsrResponseFactory $psrResponseFactory
+     * @param StreamFactoryInterface $streamFactory
      */
     public function __construct(
         EventDispatcherInterface $eventDispatcher,
         RequestParserInterface $requestParser,
         RequestProcessorInterface $requestProcessor,
-        ResponseFactoryInterface $responseFactory
+        ResponseFactoryInterface $responseFactory,
+        PsrResponseFactory $psrResponseFactory,
+        StreamFactoryInterface $streamFactory
     ) {
         $this->eventDispatcher = $eventDispatcher;
         $this->requestParser = $requestParser;
         $this->requestProcessor = $requestProcessor;
         $this->responseFactory = $responseFactory;
+        $this->psrResponseFactory = $psrResponseFactory;
+        $this->streamFactory = $streamFactory;
     }
 
     /**
      * @param RequestInterface $request
      * @return ResponseInterface
+     * @throws Throwable
      */
     public function handleRequest(RequestInterface $request): ResponseInterface
     {
@@ -93,6 +116,36 @@ class Server
             );
         }
 
-        return $this->responseFactory->createResponse($response);
+        try {
+            return $this->responseFactory->createResponse($response);
+        } catch (Throwable $throwable) {
+            if ($response instanceof ExceptionResponse) {
+                $throwable = $response->getException();
+            }
+
+            return $this->createFallbackErrorResponse($throwable);
+        }
+    }
+
+    /**
+     * @param Throwable $throwable
+     * @return ResponseInterface
+     * @throws Throwable
+     */
+    protected function createFallbackErrorResponse(Throwable $throwable): ResponseInterface
+    {
+        $httpStatusCode = 500;
+        if ($throwable instanceof StatusProvidingExceptionInterface) {
+            $httpStatusCode = $throwable->getHttpStatusCode();
+        }
+
+        return $this->psrResponseFactory->createResponse($httpStatusCode)
+            ->withBody($this->streamFactory->createStream(
+                json_encode(
+                    $this->mapException($throwable),
+                    JSON_THROW_ON_ERROR
+                )
+            ))
+            ->withHeader('Content-Type', 'application/json');
     }
 }
